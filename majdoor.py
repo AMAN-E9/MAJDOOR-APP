3483180eacbb0a295dfe826f345ce237e17a706d
AMAN <amankumarchaudhary7016@gmail.com>
2025-06-20 21:26:23 +0530

    Update majdoor.py

--- majdoor.py
+++ majdoor.py
@@ -207,13 +207,12 @@ with col2:
         st.session_state.chat_history = []
         st.rerun()
 
-# 🔊 Voice Tool Footer Panel
 st.markdown("---")
 st.markdown("### 🗣️ Majdoor Text-to-Voice Tool")
 st.markdown("""
-<iframe src="https://aman-e9.github.io/MAJDOOR-APP/tts_ui/" width="100%" height="500"></iframe>
+<iframe src="https://huggingface.co/spaces/NihalGazi/Text-To-Speech-Unlimited?embed=1" 
+        width="100%" height="650" frameborder="0"></iframe>
 """, unsafe_allow_html=True)
-st.caption("🎙 Voice generated using Majdoor's sarcasm processor")
 
 # 🪪 Footer Credit
 st.markdown(
